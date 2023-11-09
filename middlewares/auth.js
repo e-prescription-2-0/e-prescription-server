d2c3410f790249975c5a034ffa3a37bdf3f89193
Luben Sirakov <81362863+LubenSirakov@users.noreply.github.com>
2023-11-09 21:44:49 +0200

    format code (#6)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,39 +1,31 @@
-const jwt = require('../utils/jwt');
-const { userTokenName } = require('../config/app-config');
-const {userModel} = require('../models');
+const jwt = require("../utils/jwt");
+const { userTokenName } = require("../config/app-config");
+const { userModel } = require("../models");
 
 function auth(redirectUnauthenticated = true) {
-
-    return function (req, res, next) {
-        const token = req.headers[userTokenName] || '';
-        Promise.all([
-            jwt.verifyToken(token),
-            
-        ])
-            .then(([data]) => {
-               
-                userModel.findById(data.id)
-                    .then(user => {
-                        req.user = user;
-                        req.isLogged = true;
-                        next();
-                    })
-            })
-            .catch(err => {
-                if (!redirectUnauthenticated) {
-                    next();
-                    return;
-                }
-                if (['token expired', 'jwt must be provided'].includes(err.message)) {
-                    console.error(err);
-                    res
-                        .status(401)
-                        .send({ message: "Invalid token!" });
-                    return;
-                }
-                next(err);
-            });
-    }
+  return function (req, res, next) {
+    const token = req.headers[userTokenName] || "";
+    Promise.all([jwt.verifyToken(token)])
+      .then(([data]) => {
+        userModel.findById(data.id).then((user) => {
+          req.user = user;
+          req.isLogged = true;
+          next();
+        });
+      })
+      .catch((err) => {
+        if (!redirectUnauthenticated) {
+          next();
+          return;
+        }
+        if (["token expired", "jwt must be provided"].includes(err.message)) {
+          console.error(err);
+          res.status(401).send({ message: "Invalid token!" });
+          return;
+        }
+        next(err);
+      });
+  };
 }
 
 module.exports = auth;
--- router/users.js
+++ router/users.js
@@ -1,40 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {register} = require('../services/userService');
-const userModel = require('../models/userModel');
+const { register } = require("../services/userService");
+const userModel = require("../models/userModel");
 
-const User = userModel
+const User = userModel;
 
-
-
-
-router.post('/register', async (req, res) => {
-
-   
-    try {
-
-      if(Object.values(req.body).some(f => f == '')) {
-        throw new Error('All fields are required');
-
+router.post("/register", async (req, res) => {
+  try {
+    if (Object.values(req.body).some((f) => f == "")) {
+      throw new Error("All fields are required");
     }
-    if(req.body.password != req.body.repeatPassword) {
-      throw new Error ('Passwords do not match!')
+    if (req.body.password != req.body.repeatPassword) {
+      throw new Error("Passwords do not match!");
+    }
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      role,
+      profileInfo,
+    } = req.body;
+
+    const user = await register(
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      role,
+      profileInfo
+    );
+
+    res.status(201);
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
   }
-      const { firstName, lastName, username, email, password, role, profileInfo } = req.body;
-     
-
-      const user = await register(firstName, lastName, username, email, password, role, profileInfo);
-      
-     
-      
-      res.status(201);
-      res.json(user);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
-    }
-  });
-
+});
 
 router.delete("/:userId/delete", async (req, res) => {
   const userId = req.params.userId;
@@ -56,7 +61,6 @@ router.delete("/:userId/delete", async (req, res) => {
   }
 });
 
-
 router.get("/profile/:userId", async (req, res) => {
   const userId = req.params.userId;
 
@@ -77,4 +81,4 @@ router.get("/profile/:userId", async (req, res) => {
   }
 });
 
-module.exports = router
+module.exports = router;
