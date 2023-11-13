70a640eea2647fde0b9de8d6aa0a6c313ea83f7e
Warw1ck <bradvata15@gmail.com>
2023-11-13 11:43:42 +0200

    Update existing Routes and Create new Routes

--- services/doctorService.js
+++ services/doctorService.js
@@ -14,7 +14,47 @@ const getDoctorInfo = async (doctorId) => {
   return doctor;
 };
 
+const getAllPatientsFromDoctorList = async (doctorId) => {
+  const doctor = await User.findById(doctorId).populate("patients");
+
+  if (!doctor) {
+    throw new Error("Unknown doctor");
+  }
+
+  return doctor.patients;
+};
+
+const addPatientToDoctorList = async (patientId, doctorId) => {
+  const patient = await User.findById(patientId);
+  const doctor = await User.findById(doctorId);
+
+  if (!patient || !doctor) {
+    throw new Error("Unknown patient or doctor");
+  }
+
+  doctor.patients.push(doctor._id);
+  doctor.save();
+
+  return doctor.patients;
+};
+
+const removePatientFromDoctorList = async (patientId, doctorId) => {
+  const patient = await User.findById(patientId);
+  const doctor = await User.findById(doctorId);
+
+  if (!patient || !doctor) {
+    throw new Error("Unknown patient or doctor");
+  }
+
+  doctor.patients.splice(doctor.patient.indexOf(patient._id), 1);
+  doctor.save();
+  return doctor.patients;
+};
+
 module.exports = {
   getAllDoctorsInfo,
   getDoctorInfo,
+  addPatientToDoctorList,
+  removePatientFromDoctorList,
+  getAllPatientsFromDoctorList
 };
