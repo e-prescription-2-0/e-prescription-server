2e65a78c4606aeb2e7d46a2dcb0d43ca4c68f9d2
Warw1ck <bradvata15@gmail.com>
2023-11-13 11:55:39 +0200

    Fix doctorServise

--- services/doctorService.js
+++ services/doctorService.js
@@ -1,19 +1,16 @@
 const User = require("../models/userModel");
 
-const getAllDoctorsInfo = async () => {
-  const doctors = await User.find({ role: "doctor" })
+const getAllDoctorsInfo = async () => await User.find({ role: "doctor" })
     .select("firstName lastName email role specialty")
     .exec();
-  return doctors;
-};
 
-const getDoctorInfo = async (doctorId) => {
-  const doctor = await User.findById(doctorId)
+
+const getDoctorInfo = async (doctorId) =>
+  await User.findById(doctorId)
     .select("firstName lastName email role specialty")
     .exec();
-  return doctor;
-};
 
+    
 const getAllPatientsFromDoctorList = async (doctorId) => {
   const doctor = await User.findById(doctorId).populate("patients");
 
@@ -56,5 +53,5 @@ module.exports = {
   getDoctorInfo,
   addPatientToDoctorList,
   removePatientFromDoctorList,
-  getAllPatientsFromDoctorList
+  getAllPatientsFromDoctorList,
 };
