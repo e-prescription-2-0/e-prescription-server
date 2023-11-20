398d2e25d6aef4d7b1cf93d48c740f806f6c9dc0
Warw1ck <bradvata15@gmail.com>
2023-11-20 13:32:43 +0200

    Update dbURL in config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3030,
-        dbURL: 'mongodb://localhost:27017/e-v1',
+        dbURL: 'mongodb+srv://user11:user11@warwick.zwcwaek.mongodb.net/e-v1',
         origin: ['http://localhost:5173', 'http://localhost:3000']
     },
     production: {
